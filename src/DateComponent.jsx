b184f3416287c4719a782e510d3ed6cbdb0bdac4
Lesha <leshapowerlifter@gmail.com>
2023-09-07 00:13:44 +0300

    provided final changes in date

--- src/DateComponent.jsx
+++ src/DateComponent.jsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const DateComponent = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   // Use useEffect to update the date every second (or as needed)
   useEffect(() => {
-    const interval = setInterval(() => {
+    setInterval(() => {
       setCurrentDate(new Date());
     }, 1000); // Update every 1000ms (1 second)
-
-    // Cleanup the interval when the component unmounts
-    return () => clearInterval(interval);
   }, []);
 
   return (
